1fa73e3c53153f7a46826c458ce6b84a1539c296
Suyash <suyash.krishna9@gmail>
2020-11-24 13:19:46 +0400

    Put all prints on main

--- lib/game.rb
+++ lib/game.rb
@@ -9,30 +9,14 @@ class Game
     @move_num = 1
   end
 
-  attr_reader :gameover, :move_num
+  attr_reader :move_num, :play
 
   attr_writer :first_d, :second_d
 
-  def print_game
-    puts '   1 2 3 '
-    puts '  -------'
-    puts "a |#{@play[0][0]}|#{@play[0][1]}|#{@play[0][2]}|"
-    puts '  -------'
-    puts "b |#{@play[1][0]}|#{@play[1][1]}|#{@play[1][2]}|"
-    puts '  -------'
-    puts "c |#{@play[2][0]}|#{@play[2][1]}|#{@play[2][2]}|"
-    puts '  -------'
-    puts
-  end
+  attr_accessor :gameover, :current_player
 
-  def check_valid_input(input, player)
-    if input.length == 2
-      @current_player = player
-      update_d(input)
-    else
-      puts 'Wrong value entered! Please try again!'
-      puts
-    end
+  def input_valid?(input)
+    input.length == 2
   end
 
   def update_d(input)
@@ -41,17 +25,14 @@ class Game
 
     table_rows.each_with_index { |item, i| @first_d = i if input.include?(item) }
     table_columns.each_with_index { |item, i| @second_d = i if input.include?(item) }
-
-    check_valid_move
   end
 
-  def check_valid_move
-    if @play[@first_d][@second_d] != 'x' && @play[@first_d][@second_d] != 'o'
-      play_move
-    else
-      puts 'This square is already filled! Please try again!'
-      puts
-    end
+  def move_valid?
+    !@first_d.nil? && !@second_d.nil?
+  end
+
+  def square_filled?
+    @play[@first_d][@second_d] != 'x' && @play[@first_d][@second_d] != 'o'
   end
 
   def winning_possibilities
@@ -66,23 +47,14 @@ class Game
   end
 
   def play_move
-    symbol = @current_player.symbol
-    @play[@first_d][@second_d] = symbol
+    @play[@first_d][@second_d] = @current_player.symbol
     @move_num += 1
-    game_won if winning_possibilities.any? do |item|
-      item.push(symbol)
+  end
+
+  def game_won?
+    winning_possibilities.any? do |item|
+      item.push(@current_player.symbol)
       item.uniq.length == 1
     end
-  end
-
-  def game_won
-    game_over
-    print_game
-    puts "#{@current_player.name} has won the game!"
-  end
-
-  def game_over
-    @gameover = true
-    puts 'Game Over!'
   end
 end
